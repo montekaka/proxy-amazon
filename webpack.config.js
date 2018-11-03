492c4be46a0fd4a080f137d22c17ff1d82815311
Hao-Han Pan <c.avantin94@gmail.com>
2018-11-03 18:02:34 -0500

    Add webpack and babel config files

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+var SOURCE_DIR = path.resolve(__dirname, 'client/src');
+var END_DIR = path.resolve(__dirname, 'client/dist');
+
+var config = {
+  entry: SOURCE_DIR + '/index.jsx',
+  output: {
+    path: END_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js(x|)$/,
+        include: SOURCE_DIR,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
