c2c16427da6f9c405e97b233249b7eed1b78fdc9
montekaka <josh.chen.jia@gmail.com>
2018-11-06 22:18:41 -0800

    add proxy route to get data from suggestion micro-service

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const app = express();
 
+require('./proxy-suggestions')(app)
 app.use(express.static(path.join(__dirname, '/../client')));
 
 app.listen(3000, () => console.log('Now listening on port 3000'));
