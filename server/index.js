76ad761058ac00a85516f4f565263b40db889942
montekaka <josh.chen.jia@gmail.com>
2018-11-10 16:45:54 -0800

    add bodyParser

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./proxy-suggestions')(app)
 app.use(express.static(path.join(__dirname, '/../client')));
