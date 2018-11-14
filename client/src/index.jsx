d7a0364f670081b6e5c1240e09617fd7611e9dc4
Hao-Han Pan <c.avantin94@gmail.com>
2018-11-13 20:20:32 -0600

    Add reviews to page container

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Suggest from './services/product-suggestions/suggest.jsx';
+import ReviewsContainer from './services/reviews/components/ReviewsContainer.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      userID: 1, // make this dynamic later
       itemID: 1, // make this dynamic later
     }
   }
@@ -14,6 +16,7 @@ class App extends React.Component {
     return (
       <div className="container">
         <Suggest id={this.state.itemID}/>
+        <ReviewsContainer itemID={this.state.itemID} userID={this.state.itemID} />
       </div>
     );
   }
